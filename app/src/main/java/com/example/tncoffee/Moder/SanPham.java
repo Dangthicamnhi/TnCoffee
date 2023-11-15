b0c97492162ca04dff68b05d617a1094000b1201
Dangthicamnhi <dangthicamnhi12@gmail.com>
2023-11-15 10:07:01 +0700

    QL Sản Phẩm

--- app/src/main/java/com/example/tncoffee/Moder/SanPham.java
+++ app/src/main/java/com/example/tncoffee/Moder/SanPham.java
@@ -3,4 +3,24 @@ package com.example.tncoffee.Moder;
 import java.io.Serializable;
 
 public class SanPham implements Serializable {
+<<<<<<< HEAD
+=======
+    String maSanPham , TenSP ;
+
+    public String getMaSanPham() {
+        return maSanPham;
+    }
+
+    public void setMaSanPham(String maSanPham) {
+        this.maSanPham = maSanPham;
+    }
+
+    public String getTenSP() {
+        return TenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        TenSP = tenSP;
+    }
+>>>>>>> 87a532b (QL Sản Phẩm)
 }
