590378090e2e6eb892d033976deca889ecf96490
Dangthicamnhi <dangthicamnhi12@gmail.com>
2023-11-27 14:54:37 +0700

    OrDer + bill

---
+++ app/src/main/java/com/example/tncoffee/Activity/Order.java
@@ -0,0 +1,57 @@
+package com.example.tncoffee.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import com.example.tncoffee.R;
+
+public class Order extends AppCompatActivity {
+    EditText edtItemName, edtQuantity;
+    RadioGroup radgSize;
+    Spinner spTenMon;
+    Button btnOrder;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        // Ánh xạ các thành phần trong giao diện XML
+        edtItemName = findViewById(R.id.edtItemName);
+        edtQuantity = findViewById(R.id.edtQuantity);
+        radgSize = findViewById(R.id.radgSize);
+        spTenMon = findViewById(R.id.spTenMon);
+        btnOrder = findViewById(R.id.btnOrder);
+
+        btnOrder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Xử lý sự kiện khi nhấn nút "Đặt hàng"
+                String itemName = edtItemName.getText().toString();
+                String quantity = edtQuantity.getText().toString();
+                int selectedSizeId = radgSize.getCheckedRadioButtonId();
+                String size = "";
+                if (selectedSizeId != -1) {
+                    RadioButton selectedSize = findViewById(selectedSizeId);
+                    size = selectedSize.getText().toString();
+                }
+                String selectedMon = spTenMon.getSelectedItem().toString();
+
+                // TODO: Thực hiện các hành động liên quan đến đặt hàng, như gửi dữ liệu lên server, lưu vào CSDL, v.v.
+
+                // Chuyển sang trang thanh toán hóa đơn
+                goToPaymentActivity();
+            }
+        });
+    }
+
+    private void goToPaymentActivity() {
+        // TODO: Chuyển sang trang thanh toán hóa đơn (PaymentActivity)
+    }
+}
---
+++ app/src/main/java/com/example/tncoffee/Activity/Payment.java
@@ -0,0 +1,37 @@
+package com.example.tncoffee.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.example.tncoffee.R;
+
+public class Payment extends AppCompatActivity {
+   TextView tvItemName, tvQuantity, tvSize, tvSelectedMon;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment);
+
+            // Ánh xạ các thành phần trong giao diện XML
+            tvQuantity = findViewById(R.id.tvQuantity);
+            tvSize = findViewById(R.id.tvSize);
+            tvSelectedMon = findViewById(R.id.tvSelectedMon);
+
+            // Nhận dữ liệu từ trang Order
+            Bundle extras = getIntent().getExtras();
+            if (extras != null) {
+                String itemName = extras.getString("itemName");
+                String quantity = extras.getString("quantity");
+                String size = extras.getString("size");
+                String selectedMon = extras.getString("selectedMon");
+
+                // Hiển thị dữ liệu lên giao diện trang thanh toán hóa đơn
+                tvItemName.setText(itemName);
+                tvQuantity.setText(quantity);
+                tvSize.setText(size);
+                tvSelectedMon.setText(selectedMon);
+            }
+        }
+}
