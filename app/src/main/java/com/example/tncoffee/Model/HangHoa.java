7f2dbb97cecf10a52bfb9be3a2bd4b27299f100c
Dangthicamnhi <dangthicamnhi12@gmail.com>
2023-11-30 14:26:05 +0700

    QL Kho

--- app/src/main/java/com/example/tncoffee/Model/HangHoa.java
+++ app/src/main/java/com/example/tncoffee/Model/HangHoa.java
@@ -1,10 +1,11 @@
 package com.example.tncoffee.Model;
 
 import java.io.Serializable;
+import java.util.List;
+
 public class HangHoa implements Serializable {
 
     String MaHH, TenHH, Gia, SoLuong, Loai;
-    private static int cafe = 0, duong = 0, botmy = 0, tra = 0;
 
     public HangHoa(String maHH, String tenHH, String gia, String soLuong, String loai) {
         MaHH = maHH;
@@ -12,51 +13,27 @@ public class HangHoa implements Serializable {
         Gia = gia;
         SoLuong = soLuong;
         Loai = loai;
-        if (loai.equals("Cafe")) {
-            cafe++;
-        } else if (loai.equals("Đường")) {
-            duong++;
-        } else if (loai.equals("Bột Mỳ")) {
-            botmy++;
-        } else if (loai.equals("Trà")) {
-            tra++;
-        }
     }
 
-    public static void GiamSoLuong(String loai) {
-        if (loai.equals("Cafe")) {
-            cafe--;
-        } else if (loai.equals("Đường")) {
-            duong--;
-        } else if (loai.equals("Bột Mỳ")) {
-            botmy--;
-        } else if (loai.equals("Trà")) {
-            tra--;
-        }
-    }
+    public static String ThongKe(List<HangHoa> lhh) {
 
-    public static void ThayDoiSoLuong(String loaiGiam, String loaiTang) {
-        if (loaiGiam.equals("Cafe")) {
-            cafe--;
-        } else if (loaiGiam.equals("Đường")) {
-            duong--;
-        } else if (loaiGiam.equals("Bột Mỳ")) {
-            botmy--;
-        } else if (loaiGiam.equals("Trà")) {
-            tra--;
+        int cafe = 0, duong = 0, botmy = 0, tra = 0;
+        for (HangHoa sp : lhh) {
+            switch (sp.getLoai()) {
+                case "Cafe":
+                    cafe++;
+                    break;
+                case "Đường":
+                    duong++;
+                    break;
+                case "Bột My":
+                    botmy++;
+                    break;
+                case "Trà":
+                    tra++;
+                    break;
+            }
         }
-        if (loaiTang.equals("Cafe")) {
-            cafe++;
-        } else if (loaiTang.equals("Đường")) {
-            duong++;
-        } else if (loaiTang.equals("Bột Mỳ")) {
-            botmy++;
-        } else if (loaiTang.equals("Trà")) {
-            tra++;
-        }
-    }
-
-    public static String ThongKe() {
         return "Cafe: " + cafe +
                 "\nĐường: " + duong +
                 "\nBột Mỳ: " + botmy +
@@ -104,37 +81,5 @@ public class HangHoa implements Serializable {
 
     public void setLoai(String loai) {
         Loai = loai;
-    }
-
-    public static int getCafe() {
-        return cafe;
-    }
-
-    public static void setCafe(int cafe) {
-        HangHoa.cafe = cafe;
-    }
-
-    public static int getDuong() {
-        return duong;
-    }
-
-    public static void setDuong(int duong) {
-        HangHoa.duong = duong;
-    }
-
-    public static int getBotmy() {
-        return botmy;
-    }
-
-    public static void setBotmy(int botmy) {
-        HangHoa.botmy = botmy;
-    }
-
-    public static int getTra() {
-        return tra;
-    }
-
-    public static void setTra(int tra) {
-        HangHoa.tra = tra;
     }
 }
