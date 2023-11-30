03875561b6f5101a0183c94641ee8d6a8548ef56
Dangthicamnhi <dangthicamnhi12@gmail.com>
2023-11-30 14:26:46 +0700

    QL Kho

--- app/src/main/java/com/example/tncoffee/Activity/QLKho.java
+++ app/src/main/java/com/example/tncoffee/Activity/QLKho.java
@@ -85,7 +85,7 @@ public class QLKho extends AppCompatActivity {
         btnThongKe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                tvThongKeKho.setText(HangHoa.ThongKe());
+                tvThongKeKho.setText(HangHoa.ThongKe(danhSach));
             }
         });
     }
